21a94f5a42596e58bfcc68908258a3bea77657f4
Oleg <barlugov2001@mail.ru>
2023-05-18 14:03:49 +0300

    addTrainer

---
+++ script.js
@@ -0,0 +1,23 @@
+// let users = ['Ваня', 'Иштван',];
+// users.push('Оля');
+// users.splice(1, 1, 'Петя');
+// console.log(users.indexOf('Петя'));
+// const arr = users.shift(0);
+// console.log(arr);
+// users.unshift('Маша', 'Паша');
+// console.log(users);
+
+
+// let arr = ['Ваня', 'Иштван', 'Оля',];
+// let removedElement = arr.splice(1, 1);
+// console.log(removedElement);
+
+
+let str = 'Ваня,Иштван,Оля';
+let arrStr = str.split(',')
+console.log(arrStr)
+
+
+
+
+
