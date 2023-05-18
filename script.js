a2c56c2c644179f10bc104d7c802b4680a0876a1
Oleg <barlugov2001@mail.ru>
2023-05-18 14:14:26 +0300

    addTask

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 let users = ['Ваня', 'Иштван',];
 users.push('Оля');
 users.splice(1, 1, 'Петя');
-console.log(users.indexOf('Петя'));
+console.log(users.indexOf('Петя'))
+    ;
 const arr = users.shift(0);
 console.log(arr);
 users.unshift('Маша', 'Паша');
 console.log(users);
 
 
+let arr2 = ['Ваня', 'Иштван', 'Оля',];
+let removedElement = arr2.splice(1, 1);
+console.log(removedElement);
 
+
+let str = 'Ваня,Иштван,Оля';
+let arrStr = str.split(',')
+console.log(arrStr)
 
 
 
